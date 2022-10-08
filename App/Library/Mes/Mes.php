52f130e4d8f0100345fa485f52e35f855a50598a
tucno21 <carlitostucno@gmail.com>
2022-10-07 19:52:06 -0500

    convertir mes numero a letra español

---
+++ App/Library/Mes/Mes.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Library\Mes;
+
+//comverti numero mes a español
+
+class Mes
+{
+    public static function data($mes)
+    {
+        switch ($mes) {
+            case 1:
+                return 'Enero';
+                break;
+            case 2:
+                return 'Febrero';
+                break;
+            case 3:
+                return 'Marzo';
+                break;
+            case 4:
+                return 'Abril';
+                break;
+            case 5:
+                return 'Mayo';
+                break;
+            case 6:
+                return 'Junio';
+                break;
+            case 7:
+                return 'Julio';
+                break;
+            case 8:
+                return 'Agosto';
+                break;
+            case 9:
+                return 'Septiembre';
+                break;
+            case 10:
+                return 'Octubre';
+                break;
+            case 11:
+                return 'Noviembre';
+                break;
+            case 12:
+                return 'Diciembre';
+                break;
+        }
+    }
+}
