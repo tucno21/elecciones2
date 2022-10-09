0d5943c556edf9cd97913652fc345de818171016
tucno21 <carlitostucno@gmail.com>
2022-10-09 08:53:36 -0500

    permisos al sistema

--- App/View/layoutDash/menuDash.php
+++ App/View/layoutDash/menuDash.php
@@ -25,76 +25,84 @@ $menuSession = [
 //CREACION DE ENLACES PARA EL MENU SIDEBAR
 $linksSidebar = [
     ['header' => 'Navegación',],
-    [
-        'mode' => 'menu',
-        'text' => 'Dashboard',
-        'url'  => route('dashboard.index'),
-        'icon' => 'bi bi-speedometer2',
-    ],
-    [
-        'mode' => 'submenu',
-        'text'    => 'Usuarios',
-        'url'    => '#',
-        'icon' => 'bi bi-person-lines-fill',
-        'submenu' => [
-            [
-                'text' => 'Usuarios',
-                'url'  => route('users.index'),
-                'icon' => 'fas fa-circle',
+    can('dashboard.index') ?
+        [
+            'mode' => 'menu',
+            'text' => 'Dashboard',
+            'url'  => route('dashboard.index'),
+            'icon' => 'bi bi-speedometer2',
+        ] : null,
+    can('users.index')  ?
+        [
+            'mode' => 'submenu',
+            'text'    => 'Usuarios',
+            'url'    => '#',
+            'icon' => 'bi bi-person-lines-fill',
+            'submenu' => [
+                [
+                    'text' => 'Usuarios',
+                    'url'  => route('users.index'),
+                    'icon' => 'fas fa-circle',
+                ],
+                [
+                    'text' => 'Roles',
+                    'url'  => route('roles.index'),
+                    'icon' => 'fas fa-circle',
+                ],
+                [
+                    'text' => 'Permisos',
+                    'url'  => route('permissions.index'),
+                    'icon' => 'fas fa-circle',
+                ],
+                [
+                    'text' => 'Instituciones',
+                    'url'  => route('schools.index'),
+                    'icon' => 'bi bi-house-fill',
+                ],
             ],
-            [
-                'text' => 'Roles',
-                'url'  => route('roles.index'),
-                'icon' => 'fas fa-circle',
-            ],
-            [
-                'text' => 'Permisos',
-                'url'  => route('permissions.index'),
-                'icon' => 'fas fa-circle',
-            ],
-            [
-                'text' => 'Instituciones',
-                'url'  => route('schools.index'),
-                'icon' => 'bi bi-house-fill',
-            ],
-        ],
-    ],
-    [
-        'mode' => 'menu',
-        'text' => 'Mi Institución',
-        'url'  => route('schools.myschool'),
-        'icon' => 'bi bi-house-door',
-    ],
-    [
-        'mode' => 'menu',
-        'text' => 'Mesa Sufragio',
-        'url'  => route('votinggroups.index'),
-        'icon' => 'bi bi-list-task',
-    ],
-    [
-        'mode' => 'menu',
-        'text' => 'Estudiantes',
-        'url'  => route('students.index'),
-        'icon' => 'bi bi-people',
-    ],
-    [
-        'mode' => 'menu',
-        'text' => 'Roles Estudiantes',
-        'url'  => route('studentroles.index'),
-        'icon' => 'bi bi-sliders2-vertical',
-    ],
-    [
-        'mode' => 'menu',
-        'text' => 'Candidatos',
-        'url'  => route('candidates.index'),
-        'icon' => 'bi bi-person-workspace',
-    ],
-    [
-        'mode' => 'menu',
-        'text' => 'Materiales y Actas',
-        'url'  => route('actas.index'),
-        'icon' => 'bi bi-file-earmark-pdf',
-    ],
+        ] : null,
+    can('schools.myschool')  ?
+        [
+            'mode' => 'menu',
+            'text' => 'Mi Institución',
+            'url'  => route('schools.myschool'),
+            'icon' => 'bi bi-house-door',
+        ] : null,
+    can('votinggroups.index')  ?
+        [
+            'mode' => 'menu',
+            'text' => 'Mesa Sufragio',
+            'url'  => route('votinggroups.index'),
+            'icon' => 'bi bi-list-task',
+        ] : null,
+    can('students.index')  ?
+        [
+            'mode' => 'menu',
+            'text' => 'Estudiantes',
+            'url'  => route('students.index'),
+            'icon' => 'bi bi-people',
+        ] : null,
+    can('studentroles.index')  ?
+        [
+            'mode' => 'menu',
+            'text' => 'Roles Estudiantes',
+            'url'  => route('studentroles.index'),
+            'icon' => 'bi bi-sliders2-vertical',
+        ] : null,
+    can('candidates.index')  ?
+        [
+            'mode' => 'menu',
+            'text' => 'Candidatos',
+            'url'  => route('candidates.index'),
+            'icon' => 'bi bi-person-workspace',
+        ] : null,
+    can('actas.index')  ?
+        [
+            'mode' => 'menu',
+            'text' => 'Materiales y Actas',
+            'url'  => route('actas.index'),
+            'icon' => 'bi bi-file-earmark-pdf',
+        ] : null,
 ];
 
 
